448422e68bf51d943631312c4255661465453036
Prince Gondaliya <100784026+Princepatel20@users.noreply.github.com>
2024-08-28 14:06:55 +0530

    Update experiment3.sol

--- Blockchain Technology/experiment3.sol
+++ Blockchain Technology/experiment3.sol
@@ -1,1 +1,22 @@
-
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+contract PrinceGondaliya3 {
+    string public prince_name;
+    uint public prince_age;
+    bool public prince_isStudent;
+    constructor() {
+        prince_name = "Prince";
+        prince_age = 21;
+        prince_isStudent = true;
+    }
+    function setDetails_external(string memory _name, uint _age, bool _isStudent) external {
+        prince_name = _name;
+        prince_age = _age;
+        prince_isStudent = _isStudent;
+    }
+    function setDetails_public(string memory _name, uint _age, bool _isStudent) public {
+        prince_name = _name;
+        prince_age = _age;
+        prince_isStudent = _isStudent;
+    }
+}
