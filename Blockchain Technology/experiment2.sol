37fb88eac2b78304671269bdf63fbd190df2087f
Prince Gondaliya <100784026+Princepatel20@users.noreply.github.com>
2024-08-28 14:04:02 +0530

    Create experiment2.sol

---
+++ Blockchain Technology/experiment2.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+contract prince_gondaliya2 {
+// Unsigned integer (8 bits)
+uint8 public prince_GondaliyamyUint8 = 255;
+// Signed integer (8 bits)
+int8 public prince_Gondaliya_myInt8 = -128;
+// Boolean
+bool public prince_Gondaliya_myBool = true;
+// Address
+address public prince_Gondaliya_myAddress = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+// String
+string public prince_Gondaliya_myString = "Hello, Prince!";
+// Fixed-size byte array (2 bytes)
+bytes2 public prince_Gondaliya_myBytes2 = 0xffff;
+// Fixed-size array
+uint8[3] public prince_Gondaliya_myFixedArray = [1, 2, 3];
+// Dynamic-size array
+string[] public prince_Gondaliya_myDynamicArray = ["Solidity", "Ethereum", "Blockchain"];
+}
